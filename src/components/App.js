3f9c60e408c3bd08cfb88bbed565bfb52004de4e
gogolab <m4ttek@gmail.com>
2019-02-04 13:56:03 +0100

    added users request action to app component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { getUsersRequest } from "../actions/users";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.props.getUsersRequest();
+    }
+
     render() {
         return (
             <div className="App">
@@ -10,4 +19,9 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(
+    null,
+    {
+        getUsersRequest
+    }
+)(App);
